7327e56a043b976ea78f43bfd25cf4e108f729bb
Shalda <zeppelin.ua@gmail.com>
2016-10-13 17:27:51 +0300

    mobile form fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,30 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
-    sourcemaps = require('gulp-sourcemaps');
-
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create();
 gulp.task('sass', function () {
     return gulp.src('./frontend/stylesheets/**/*.scss')
         .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-            .pipe(autoprefixer({
-                browsers: ['last 15 versions'],
-                cascade: false
-            }))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(browserSync.stream())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./frontend/stylesheets/**/*.scss', ['sass']);
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+   gulp.watch('./frontend/stylesheets/**/*.scss', ['sass']);
+   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+gulp.task('default',['sass', 'browser-sync']);
