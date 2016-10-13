af3d92a26bbf2670a12a34fb1bac0af15f5e6291
Shalda <zeppelin.ua@gmail.com>
2016-10-13 03:27:20 +0300

    + slider ready!

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 $(document).ready(function () {
     //initialize swiper when document ready
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
+
         freeMode: true,
+        slidesPerView: "auto",
         scrollbar: '.swiper-scrollbar',
         scrollbarHide: false,
         pagination: '.swiper-pagination',
+        paginationHide: true,
         paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         breakpoints: {
-            900: {
+            650: {
                 freeMode: false,
-                slidesPerView:'none',
-                scrollbar: false
+                scrollbar: false,
+                slidesPerView: 1,
+                spaceBetweenSlides: 30
+
             }
         }
     });
-    $('.burger-menu').click(function(e){
+    $('.burger-menu').click(function (e) {
         e.preventDefault();
         $(this)
-
             .parent('.header__menu')
             .find('.header__menu__nav')
             .slideToggle();
-    })
+    });
     jQuery('#scrollup img').mouseover(function () {
         jQuery(this).animate({opacity: 0.65}, 100);
     }).mouseout(function () {
